6cd78faa8f194267ba1696c0c17a8d83720f2a7a
Juergen Schilling <juergen_schilling@web.de>
2022-05-15 20:58:06 +0200

    Fix basic login

--- Levoit/module.php
+++ Levoit/module.php
@@ -160,8 +160,8 @@ declare(strict_types=1);
 
             // extract token
             $json_response = json_decode($response, true);
-            $this->_log($this->module_name,
-                'Info: The login response is %s...', $json_response);
+            $this->_log($this->module_name, sprintf(
+                'Info: The login response is %s...', $json_response));
 
             $this->token = isset($json_response['token']) ? $json_response['token'] : false;
             $this->account_id = isset($json_response['accountID']) ? $json_response['accountID'] : false;
